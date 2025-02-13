0a81e8e935a8757f9c9a18d2c944c52fcfee7a39
thisiscindy24 <cindyannwu@gmail.com>
2025-02-13 02:42:49 +0000

    done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  get("/dice/:number_of_dice/:how_many_sides", { :controller => "dice", :action => "flexible" })
 
+  get("/", {:controller => "dice", :action=> "homepage"})
 end
